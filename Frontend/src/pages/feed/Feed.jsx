6c4684c92a22fb7a1847e05f68c40d66b7418b86
Ahsan Mansoor <ahsanmansoor@Ahsans-MacBook-Air.local>
2023-10-04 11:10:08 -0600

    Update

---
+++ Frontend/src/pages/feed/Feed.jsx
@@ -0,0 +1,15 @@
+import { useNavigate, useParams } from "react-router-dom";
+import AuthConsumer from "../../components/AuthContext";
+import InfinitePostsLayout from "../../components/InfinitePosts";
+
+export function Feed() {
+  const { isAuthenticated } = AuthConsumer();
+  const navigate = useNavigate();
+  const { feedName } = useParams();
+  if (feedName == "home" && !isAuthenticated) {
+    return navigate("/login");
+  }
+  return <InfinitePostsLayout linkUrl={`posts/${feedName || "all"}`} apiQueryKey={feedName} />;
+}
+
+export default Feed;
